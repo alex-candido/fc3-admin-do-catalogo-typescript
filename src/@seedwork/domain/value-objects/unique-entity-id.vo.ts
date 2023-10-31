b7dd9dec0168db43e456915549a4e04e48169e6b
alex.candido.tec <alex.candido.tec@gmail.com>
2023-10-31 04:39:23 +0000

    criando abstracao para o value objects

--- src/@seedwork/domain/value-objects/unique-entity-id.vo.ts
+++ src/@seedwork/domain/value-objects/unique-entity-id.vo.ts
@@ -1,14 +1,15 @@
 import { v4 as uuidv4, validate as uuidValidate } from "uuid";
 import InvalidUuidError from "../errors/invalid-uuid.error";
+import ValueObject from "./value-object";
 
-export class UniqueEntityId {
+export class UniqueEntityId extends ValueObject<string> {
   constructor(readonly id?: string) {
-    this.id = id || uuidv4();
+    super(id || uuidv4());
     this.validate();
   }
 
   private validate() {
-    const isValid = uuidValidate(this.id);
+    const isValid = uuidValidate(this.value);
     if (!isValid) {
       throw new InvalidUuidError();
     }
