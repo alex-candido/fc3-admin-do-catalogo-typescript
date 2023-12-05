fd3112f8852e85b67c3bc65d5b538465c07ca028
Alex Cândido <alex.candido.tec@gmail.com>
2023-12-05 12:03:23 -0300

    registrando casos de uso e repositorios de categoria no nestjs

--- src/nestjs/src/categories/categories.controller.ts
+++ src/nestjs/src/categories/categories.controller.ts
@@ -1,4 +1,8 @@
 import {
+  CreateCategoryUseCase,
+  ListCategoriesUseCase,
+} from '@fc/micro-videos/category/application';
+import {
   Body,
   Controller,
   Delete,
@@ -13,16 +17,22 @@ import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Controller('categories')
 export class CategoriesController {
-  constructor(private readonly categoriesService: CategoriesService) {}
+  constructor(
+    private readonly categoriesService: CategoriesService,
+    private createUseCase: CreateCategoryUseCase.UseCase,
+    private listUseCase: ListCategoriesUseCase.UseCase,
+  ) {}
 
   @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {
-    return this.categoriesService.create(createCategoryDto);
+  create(@Body() _createCategoryDto: CreateCategoryDto) {
+    return this.createUseCase.execute({ name: 'alex candido' });
+    // return this.categoriesService.create(createCategoryDto);
   }
 
   @Get()
   findAll() {
-    return this.categoriesService.findAll();
+    return this.listUseCase.execute({});
+    // return this.categoriesService.findAll();
   }
 
   @Get(':id')
--- src/nestjs/src/categories/categories.module.ts
+++ src/nestjs/src/categories/categories.module.ts
@@ -1,9 +1,35 @@
+import {
+  CreateCategoryUseCase,
+  ListCategoriesUseCase,
+} from '@fc/micro-videos/category/application';
+import { CategoryRepository } from '@fc/micro-videos/category/domain';
+import { CategoryInMemoryRepository } from '@fc/micro-videos/category/infra';
 import { Module } from '@nestjs/common';
-import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
+import { CategoriesService } from './categories.service';
 
 @Module({
   controllers: [CategoriesController],
-  providers: [CategoriesService],
+  providers: [
+    CategoriesService,
+    {
+      provide: 'CategoryInMemoryRepository',
+      useClass: CategoryInMemoryRepository,
+    },
+    {
+      provide: CreateCategoryUseCase.UseCase,
+      useFactory: (CategoryRepo: CategoryRepository.Repository) => {
+        return new CreateCategoryUseCase.UseCase(CategoryRepo);
+      },
+      inject: ['CategoryInMemoryRepository'],
+    },
+    {
+      provide: ListCategoriesUseCase.UseCase,
+      useFactory: (CategoryRepo: CategoryRepository.Repository) => {
+        return new ListCategoriesUseCase.UseCase(CategoryRepo);
+      },
+      inject: ['CategoryInMemoryRepository'],
+    },
+  ],
 })
 export class CategoriesModule {}
