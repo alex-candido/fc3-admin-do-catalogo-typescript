c91e9c2bb85c49c463ac6a313ee78579c5ec09a4
Alex Cândido <alex.candido.tec@gmail.com>
2024-01-20 14:31:54 -0300

    trabalhando integração umzung e configurando sistema de migrations e sequelize

--- src/nestjs/test/migrate.e2e-spec.ts
+++ src/nestjs/test/migrate.e2e-spec.ts
@@ -1,28 +1,32 @@
-import { INestApplication } from '@nestjs/common';
+import { migrator } from '@fc/micro-videos/@seedwork/infra';
+import { getConnectionToken } from '@nestjs/sequelize';
 import { Test, TestingModule } from '@nestjs/testing';
-import request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Umzug } from 'umzug';
+import { MigrationModule } from '../src/database/migration/migration.module';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+describe('Migrate (e2e)', () => {
+  let umzug: Umzug;
+  const totalMigrations = 1;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [MigrationModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
+    const sequelize = moduleFixture.get(getConnectionToken());
+
+    umzug = migrator(sequelize, { logger: undefined });
   });
 
-  it('/ (GET)', done => {
-    request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!')
-      .end((err, _res) => {
-        if (err) return done(err);
-        return done();
-      });
+  test('up command', async () => {
+    await umzug.down({ to: 0 as any });
+    const result = await umzug.up();
+    expect(result).toHaveLength(totalMigrations);
+  });
+
+  test('down command', async () => {
+    await umzug.up();
+    const result = await umzug.down({ to: 0 as any });
+    expect(result).toHaveLength(totalMigrations);
   });
 });
